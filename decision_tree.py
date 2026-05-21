964fcbbc9df911301efa560f98c448685f157863
elbitor0 <123994826+elbitor0@users.noreply.github.com>
2026-05-22 01:02:17 +0200

    feature: fix gini_inpurity

--- decision_tree.py
+++ decision_tree.py
@@ -3,18 +3,19 @@ from tqdm import tqdm
 
 class Decision_Tree:
     
-    def __init__(self,name, left=None, right=None, target_repartition = {}):
-        self.name = name
+    def __init__(self,label, left=None, right=None, target_repartition = {}):
+        self.label = label
         self.left = left
         self.right = right
+        
+        # Dictionnaire: key= Target; value = (True qte, False qte)
         self.target_repartition = target_repartition
 
-    def total_target_nb(self):
-        return sum(self.target_repartition.values())
-    
-    def target_gini_impurity(self,target):
+    def total_repartition_nb(self):
+        return sum(true_qte+false_qte for (true_qte,false_qte) in self.target_repartition.values())
+    def gini_impurity(self,target):
         if self.left== None and self.right ==None:
-            return Leaves(self).target_gini_impurity(target)
+            return Leaves(self).gini_impurity(target)
         else:
             left = self.left
             right = self.right
@@ -26,6 +27,6 @@ class Decision_Tree:
             return left.target_gini_impurity(target) * left_weight + right.target_gini_impurity(target) * right_weight
 class Leaves(Decision_Tree):
     
-    def target_gini_impurity(self,target):
+    def gini_impurity(self,target):
         total_nb_values = self.total_target_nb()
         return 1 - (self.target_repartition[target]/total_nb_values)**2 - ((total_nb_values-self.target_repartition[target])/total_nb_values)**2
